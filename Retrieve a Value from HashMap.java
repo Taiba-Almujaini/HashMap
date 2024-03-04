96e34dff7899e69e918a497447c51e58bd372127
Taiba-Almujaini <158799153+Taiba-Almujaini@users.noreply.github.com>
2024-03-04 12:10:23 +0400

    Retrieve a Value from HashMap.java

--- Retrieve a Value from HashMap.java
+++ Retrieve a Value from HashMap.java
@@ -4,24 +4,24 @@ import java.util.HashMap;
 public class StudentMap {
     public static void main(String[] args) {
         // Create a HashMap to store student IDs and names
-        HashMap<Integer, String> StudentMap = new HashMap<>();
+        HashMap<Integer, String> studentMap = new HashMap<>();
 
         // Add student entries to the HashMap
-        StudentMap.put(1234, "Taiba");
-        StudentMap.put(1235, "Afnan");
-        StudentMap.put(1236, "Nourah");
-        StudentMap.put(1237, " Shimaa");
-        StudentMap.put(1238, " Sara");
+        studentMap.put(1234, "Taiba");
+        studentMap.put(1235, "Afnan");
+        studentMap.put(1236, "Nourah");
+        studentMap.put(1237, " Shimaa");
+        studentMap.put(1238, " Sara");
 
 
         System.out.println("Student ID :  Student Name");
 
-        for (Integer Id : StudentMap.keySet()) {
-            System.out.println(Id + "\t\t" + StudentMap.get(Id));
+        for (Integer Id : studentMap.keySet()) {
+            System.out.println(Id + "\t\t" + studentMap.get(Id));
         }
 
         int studentID = 1234; // Retrieve a student's name using their student ID :1234 .
-        String studentName = getStudentName(StudentMap, studentID);
+        String studentName = getStudentName(studentMap, studentID);
         if (studentName != null) {
             System.out.println("Student name for ID " + studentID + ": " + studentName);
         } else {
@@ -33,4 +33,4 @@ public class StudentMap {
     public static String getStudentName(HashMap<Integer, String> map, int studentID) {
         return map.get(studentID);
     }
-    }
+}
